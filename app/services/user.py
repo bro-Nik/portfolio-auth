b464cc8db0d169bbd251f89c3d0928dd1f836155
Nikita Markin <mr.nik2014@gmail.com>
2026-01-07 15:27:59 +0300

    refactor: user service migration to SQLAlchemy

--- app/services/user.py
+++ app/services/user.py
@@ -1,19 +1,28 @@
-from typing import Optional
-from app.repositories.user import UserRepository
-from app.models.user import User
+from typing import List, Optional
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import User
+from app.repositories import UserRepository
+
 
 class UserService:
     """Сервис для работы с пользователями"""
 
-    def __init__(self, user_repo: UserRepository):
-        self.user_repo = user_repo
+    def __init__(self, db: AsyncSession):
+        self.repo = UserRepository(db)
 
     async def get_user_by_id(self, user_id: int) -> Optional[User]:
         """Получение пользователя по ID"""
-        user = await self.user_repo.get_user_by_id(user_id)
+        user = await self.repo.get(user_id)
         return user
 
     async def get_user_by_email(self, email: str) -> Optional[User]:
         """Получение пользователя по email"""
-        user = await self.user_repo.get_user_by_email(email)
+        user = await self.repo.get_by_email(email)
         return user
+
+    async def get_users(self) -> List[User]:
+        """Получение списка пользователей"""
+        users = await self.repo.get_all()
+        return users
